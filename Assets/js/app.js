fa44660f256f411ec86c573a2dfa0daa41ae6f31
Chrstjan <ps5christian@hotmail.com>
2023-11-30 08:04:31 +0100

    Started making JavaScript

---
+++ Assets/js/app.js
@@ -0,0 +1,11 @@
+const gallery = document.getElementById("gallery");
+
+const imagesArray = ['Choose-01.jpg', 'Choose-02.jpg', 'Choose-03.jpg', 'Choose-04.jpg',];
+
+const baseUrl = './Assets/images/GFX/';
+
+const createGallery = () => {
+    imagesArray.forEach((img) => {
+        
+    }) 
+}
