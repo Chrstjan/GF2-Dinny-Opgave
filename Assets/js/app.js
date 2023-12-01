41af2f37e8bb6506b6e7c287d33ca021c53b12c6
Christian <ps5christian@hotmail.com>
2023-12-01 20:24:34 +0100

    Made some styling change with the desktop version

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,56 +1,87 @@
 const gallery = document.getElementById("gallery");
+const galleryHeader = document.createElement("header");
+galleryHeader.classList.add("gallery-header");
+galleryHeader.innerHTML = "<h2>Dinny Gallery</h2>";
+gallery.appendChild(galleryHeader);
 
-const dinnyArray = ['Dinny-01.jpg', 'Dinny-02.jpg', 'Dinny-03.jpg', 'Dinny-04.jpg', 'Dinny-05.jpg', 'Dinny-06.jpg', 'Dinny-07.jpg', 'Dinny-08.jpg',];
+const dinnyArray = [
+  "Dinny-01.jpg",
+  "Dinny-02.jpg",
+  "Dinny-03.jpg",
+  "Dinny-04.jpg",
+  "Dinny-05.jpg",
+  "Dinny-06.jpg",
+  "Dinny-07.jpg",
+  "Dinny-08.jpg",
+];
 
-const chooseImagesArray = ['Choose-01.jpg', 'Choose-02.jpg', 'Choose-03.jpg', 'Choose-04.jpg',];
+const chooseImagesArray = [
+  "Choose-01.jpg",
+  "Choose-02.jpg",
+  "Choose-03.jpg",
+  "Choose-04.jpg",
+];
 
-const baseUrl = './Assets/images/GFX/';
+const baseUrl = "./Assets/images/GFX/";
 
 const createGallery = () => {
-    dinnyArray.forEach((img, index) => {
-        //Helper function
-        const createHoverElement = (headerText, text, footerText) => {
-            const figCaption = document.createElement("figcaption");
-            figCaption.classList.add("dinny-figcaption");
-            const figureHeader = document.createElement("header");
-            const figureHeaderText = document.createElement("h3");
-            figureHeaderText.textContent = headerText;
-            figureHeader.appendChild(figureHeaderText);
-            figCaption.appendChild(figureHeader);
+  dinnyArray.forEach((img, index) => {
+    //Helper function
+    const createHoverElement = (headerText, text, footerText) => {
+      const figCaption = document.createElement("figcaption");
+      figCaption.classList.add("dinny-figcaption");
+      const figureHeader = document.createElement("header");
+      const figureHeaderText = document.createElement("h3");
+      figureHeaderText.textContent = headerText;
+      figureHeader.appendChild(figureHeaderText);
+      figCaption.appendChild(figureHeader);
 
-            const figureTextContainer = document.createElement("div");
-            const figureText = document.createElement("p");
-            const figureTextTwo = document.createElement("p");
-            figureText.textContent = text;
-            figureTextTwo.textContent = text;
-            figureTextContainer.appendChild(figureText);
-            figureTextContainer.appendChild(figureTextTwo);
-            
-            const figureFooterContainer = document.createElement("footer");
-            const figureFooterText = document.createElement("a");
-            figureFooterText.textContent = footerText;
-            figureFooterContainer.appendChild(figureFooterText);
-            figureTextContainer.appendChild(figureFooterContainer);
+      const figureTextContainer = document.createElement("div");
+      const figureText = document.createElement("p");
+      const figureTextTwo = document.createElement("p");
+      figureText.textContent = text;
+      figureTextTwo.textContent = text;
+      figureTextContainer.appendChild(figureText);
+      figureTextContainer.appendChild(figureTextTwo);
 
-            figCaption.appendChild(figureTextContainer);
-            dinnyFigure.appendChild(figCaption);
-            return figCaption;
-        }
+      const figureFooterContainer = document.createElement("footer");
+      const figureFooterText = document.createElement("a");
+      figureFooterText.textContent = footerText;
+      figureFooterContainer.appendChild(figureFooterText);
+      figureTextContainer.appendChild(figureFooterContainer);
 
-        const dinnyFigure =  document.createElement("figure");
-        dinnyFigure.classList.add("dinny-figure");
-        const dinnyImages = document.createElement("img");
-        const dinnyImagesSrc = baseUrl + img;
-        dinnyImages.src = dinnyImagesSrc;
+      figCaption.appendChild(figureTextContainer);
+      dinnyFigure.appendChild(figCaption);
+      return figCaption;
+    };
 
-        dinnyFigure.appendChild(dinnyImages);
-        gallery.appendChild(dinnyFigure);
+    const dinnyFigure = document.createElement("figure");
+    dinnyFigure.classList.add("dinny-figure");
+    const dinnyImages = document.createElement("img");
+    const dinnyImagesSrc = baseUrl + img;
+    dinnyImages.src = dinnyImagesSrc;
 
-        const headerTexts = ['Gallery One', 'Gallery Two', 'Gallery Three', 'Gallery Four', 'Gallery Five', 'Gallery Six', 'Gallery Seven', 'Gallery Eight'];
+    dinnyFigure.appendChild(dinnyImages);
+    gallery.appendChild(dinnyFigure);
 
-        const figureCaption = createHoverElement(headerTexts[index], 'Hic alias laborum nesciunt ipsa, quidem aut fugit similique beatae, vero ullam eum suscipit assumenda fugiat pariatur optio repudiandae laboriosam eos expedita.', 'More:');
-        dinnyFigure.appendChild(figureCaption);
-    }); 
-}
+    const headerTexts = [
+      "Gallery One",
+      "Gallery Two",
+      "Gallery Three",
+      "Gallery Four",
+      "Gallery Five",
+      "Gallery Six",
+      "Gallery Seven",
+      "Gallery Eight",
+    ];
+
+    const figureCaption = createHoverElement(
+      headerTexts[index],
+      "Hic alias laborum nesciunt ipsa, quidem aut fugit similique beatae, vero ullam eum suscipit assumenda fugiat pariatur optio repudiandae laboriosam eos expedita.",
+      "More:"
+    );
+    dinnyFigure.appendChild(figureCaption);
+  });
+};
 
 createGallery();
