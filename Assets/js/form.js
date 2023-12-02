bdbf83ddb785be1bb3a19d3d22e85bb752b74bb6
Christian <ps5christian@hotmail.com>
2023-12-02 22:19:34 +0100

    Created the contact form and made the styling

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -8,19 +8,7 @@ const contactForm = () => {
   const contactFieldset = document.createElement("fieldset");
 
   const closeContactBtn = document.createElement("span");
-  closeContactBtn.innerHTML = "&times;";
-
-  //Append children function
-  const appendChildren = (parent, elements) => {
-    elements.forEach((element) => {
-      parent.appendChild(element);
-    });
-  };
-
-  appendChildren(contactFieldset, [closeContactBtn]);
-
-  contactFormElement.appendChild(contactFieldset);
-  contactContainer.appendChild(contactFormElement);
+  closeContactBtn.innerHTML = "<a>&times;</a>";
 
   //Helper Functions
   const createContactLabelElement = (text, forInput) => {
@@ -38,13 +26,68 @@ const contactForm = () => {
     return input;
   };
 
-  const createContactInputButtonElement = (type, className, text) => {
+  const createContactInputButtonElement = (type, className, value) => {
     const inputBtn = document.createElement("input");
     inputBtn.setAttribute("type", type);
     inputBtn.classList.add(className);
-    inputBtn.textContent = text;
+    inputBtn.setAttribute("value", value);
     return inputBtn;
   };
+
+  const fNameLabel = createContactLabelElement("First Name", "fName");
+  const fName = createContactInputElement("text", "fName", "John");
+
+  const lNameLabel = createContactLabelElement("Last Name", "lName");
+  const lName = createContactInputElement("text", "lName", "Doe");
+
+  const emailLabel = createContactLabelElement("Email", "email");
+  const email = createContactInputElement("email", "email", "none@fake.com");
+
+  const messageLabel = createContactLabelElement("Message", "messageArea");
+  const message = document.createElement("textarea");
+  message.classList.add("messageArea");
+  message.setAttribute("rows", 7);
+  message.setAttribute("placeholder", "Message");
+
+  const resetBtn = createContactInputButtonElement(
+    "reset",
+    "resetBtn",
+    "Reset"
+  );
+
+  const submitBtn = createContactInputButtonElement(
+    "submit",
+    "submitBtn",
+    "Send"
+  );
+
+  const contactBtnContainer = document.createElement("span");
+  contactBtnContainer.classList.add("btn-container");
+  contactBtnContainer.appendChild(resetBtn);
+  contactBtnContainer.appendChild(submitBtn);
+
+  //Append children function
+  const appendChildren = (parent, elements) => {
+    elements.forEach((element) => {
+      parent.appendChild(element);
+    });
+  };
+
+  appendChildren(contactFieldset, [
+    closeContactBtn,
+    fNameLabel,
+    fName,
+    lNameLabel,
+    lName,
+    emailLabel,
+    email,
+    messageLabel,
+    message,
+    contactBtnContainer,
+  ]);
+
+  contactFormElement.appendChild(contactFieldset);
+  contactContainer.appendChild(contactFormElement);
 
   closeContactBtn.addEventListener("click", () => {
     contactContainer.innerHTML = "";
