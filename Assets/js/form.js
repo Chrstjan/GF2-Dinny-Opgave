65191b78268385044a2f41bec5dcb814a8332dc5
Christian <ps5christian@hotmail.com>
2023-12-02 23:31:16 +0100

    Started making form validation

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -49,6 +49,58 @@ const contactForm = () => {
   message.setAttribute("rows", 7);
   message.setAttribute("placeholder", "Message");
 
+  const validateInput = (input, regEx, errorMessage) => {
+    const trimmedValue = input.value.trim();
+    const isValid = regEx.test(trimmedValue);
+
+    if (isValid) {
+      input.classList.add("valid");
+      input.classList.remove("invalid");
+    } else {
+      input.classList.add("invalid");
+      input.classList.remove("valid");
+      displayErrorMessage(errorMessage);
+    }
+
+    return isValid;
+  };
+
+  const displayErrorMessage = (message) => {
+    errorTextContainer.textContent = message;
+  };
+
+  const formValidation = (e) => {
+    const nameRegExp = /^[a-zA-Z]{2,17}$/;
+    const emailRegExp = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/;
+
+    const isFNameValid = validateInput(
+      fName,
+      nameRegExp,
+      "First Name must be at least 2 characters"
+    );
+    const isLNameValid = validateInput(
+      lName,
+      nameRegExp,
+      "Last Name must be at least 2 characters"
+    );
+    const isEmailValid = validateInput(
+      email,
+      emailRegExp,
+      "Email must be a valid email"
+    );
+
+    if (isFNameValid && isLNameValid && isEmailValid) {
+      contactFieldset.innerHTML = "";
+      const recivedMessage = document.createElement("h2");
+      recivedMessage.textContent =
+        "Message recived. Thank you for your message";
+      contactFieldset.appendChild(closeContactBtn);
+      contactFieldset.appendChild(recivedMessage);
+    } else {
+      e.preventDefault();
+    }
+  };
+
   const resetBtn = createContactInputButtonElement(
     "reset",
     "resetBtn",
@@ -65,6 +117,11 @@ const contactForm = () => {
   contactBtnContainer.classList.add("btn-container");
   contactBtnContainer.appendChild(resetBtn);
   contactBtnContainer.appendChild(submitBtn);
+
+  submitBtn.addEventListener("click", formValidation);
+
+  const errorTextContainer = document.createElement("div");
+  errorTextContainer.classList.add("errorContainer");
 
   //Append children function
   const appendChildren = (parent, elements) => {
@@ -84,6 +141,7 @@ const contactForm = () => {
     messageLabel,
     message,
     contactBtnContainer,
+    errorTextContainer,
   ]);
 
   contactFormElement.appendChild(contactFieldset);
